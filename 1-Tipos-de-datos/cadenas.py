05ab6fc644b0b99d2a279c50f7ccd23d8e37b664
ygperdomo <haisem8703@gmal.com>
2023-12-01 00:13:11 -0500

    Se agrega el trabajo con la cadenas, indices y métodos de cadena

---
+++ 1-Tipos-de-datos/cadenas.py
@@ -0,0 +1,23 @@
+print("Hola \nmundo")
+
+print(len("hola"))
+
+micadena = "hola mundo"
+print(micadena[3])
+print(micadena[-1])  # Indexado inverso
+print(micadena[0:7:2])
+
+name = "Pam"
+ultimas_letras = name[1:]
+print('p' + ultimas_letras)
+
+letra = 'z'
+letra = letra * 10
+print(letra)
+
+x = 'hola mundo'
+x = x.upper()
+print(x)
+
+x = x.split('O')
+print(x)
